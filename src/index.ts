066a48d958f5a6b6e93c783c15c5a487080a84ec
Baraa Khamaysa <engbaraakhamaysa@gmail.com>
2026-09-14 16:16:48 +0300

    feat: add command registry

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
-import { readConfig, setUser } from "./config.js";
+import {
+  handlerLogin,
+  registerCommand,
+  runCommand,
+  type CommandsRegistry,
+} from "./commands.js";
 
-function main() {
-  setUser("Baraa");
+// Initializes the command registry and handles CLI input
+function main(): void {
+  const registry: CommandsRegistry = {};
 
-  const config = readConfig();
+  // Registers the login command and its handler
+  registerCommand(registry, "login", handlerLogin);
 
-  console.log(config);
+  // Gets the command-line arguments without the Node.js and npm arguments
+  const args = process.argv.slice(2);
+
+  // Makes sure at least one command was provided.
+  if (args.length < 1) {
+    console.error("Error: not enough arguments");
+    process.exit(1);
+  }
+
+  // Separates the command name from its arguments
+  const cmdName = args[0];
+  const cmdArgs = args.slice(1);
+
+  // Runs the command and handles any errors
+  try {
+    runCommand(registry, cmdName, ...cmdArgs);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
 
 main();
