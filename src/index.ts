05d83c620aeaa797736c4ae7be50bd25d368a53d
Baraa Khamaysa <engbaraakhamaysa@gmail.com>
2026-09-14 14:39:18 +0300

    feat: add config management

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+import { readConfig, setUser } from "./config.js";
+
 function main() {
-  console.log("Hello, world!");
+  setUser("Baraa");
+
+  const config = readConfig();
+
+  console.log(config);
 }
 
 main();
